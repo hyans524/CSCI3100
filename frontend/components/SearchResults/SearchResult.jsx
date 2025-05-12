1fcd0faba9d1adb0e35c09af3498b9015e7c458b
Supersumanov <yanzeyang39@gmail.com>
2025-05-13 01:11:41 +0800

    search

--- frontend/components/SearchResults/SearchResult.jsx
+++ frontend/components/SearchResults/SearchResult.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './SearchResult.css'
-
+import { tripApi } from '../../src/utils/api'
 const SearchResult = ({result}) => {
   const navigate = useNavigate()
 
-  const handleClick = () => {
-    navigate('/mytrip')
-  }
+const handleClick = async () => {
+  navigate('/mytrip')
+};
   return (
     <div className="search-result" onClick={handleClick}>
         {result.destination}
